1eed2b386fa403af68217cd6096dc1aa6cf4b56c
mew7wo <mew7wo@gmail.com>
2013-04-16 13:54:32 +0800

    ds

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#coding=utf-8
+#file Name: main.py
+#author: mew7wo
+#mail: mew7wo@gmail.com
+#created Time: Sat 23 Mar 2013 08:27:06 PM CST
+
+
+from server import app as application
